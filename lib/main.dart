b21034c3796245798adb9c3b56f9ad9cfaa4d3fc
Anton Toneyev <aatoneyev@gmail.com>
2022-02-19 17:52:35 +0300

    basic home page transition to seminar page sliver app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,108 +1,134 @@
 import 'package:flutter/material.dart';
+import 'package:philosophy_flutter/SeminarPage.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(PhilosophyApp(
+      items: List<ListItem>.generate(
+          100,
+          (index) =>
+              SeminarListTile('Seminar $index', 'seminar subtitle $index')),
+    ));
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class PhilosophyApp extends StatelessWidget {
+  const PhilosophyApp({Key? key, required this.items}) : super(key: key);
 
-  // This widget is the root of your application.
+  final double appBarHeight = 66.0;
+
+  final List<ListItem> items;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              centerTitle: true,
+              title: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text('PhilosophyApp'),
+                ],
+              ),
+              pinned: true,
+              expandedHeight: 100,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Center(
+                    child: Container(
+                  padding: const EdgeInsets.all(20),
+                  color: Colors.green,
+                  alignment: Alignment.bottomCenter,
+                  child: const Text(
+                    'TeacherName',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                )),
+              ),
+              actions: [
+                IconButton(
+                  icon: const Icon(Icons.settings),
+                  onPressed: () {},
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            const Text('Hello, World!'
-            ),
+            SliverList(
+                delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                final item = items[index];
+                return ListTile(
+                  title: item.buildTitle(context),
+                  subtitle: item.buildSubtitle(context),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SeminarPage()));
+                  },
+                );
+              },
+              childCount: items.length,
+            ))
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  /*@override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(title: Text('Philosophy'),
+                actions: [IconButton(
+                  icon: const Icon(Icons.settings),
+                  onPressed: () {
+
+                  },
+
+                ),
+                ]
+            ),
+            body: ListView.builder(
+              // Let the ListView know how many items it needs to build.
+              itemCount: items.length,
+              // Provide a builder function. This is where the magic happens.
+              // Convert each item into a widget based on the type of item it is.
+              itemBuilder: (context, index) {
+                final item = items[index];
+
+                return ListTile(
+                  title: item.buildTitle(context),
+                  subtitle: item.buildSubtitle(context),
+                  onTap: (){
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const SeminarPage())
+                    );
+                  },
+                );
+              },
+            )
+        )
+    );
+  }*/
+}
+
+abstract class ListItem {
+  Widget buildTitle(BuildContext context);
+
+  Widget buildSubtitle(BuildContext context);
+}
+
+class SeminarListTile implements ListItem {
+  final String title;
+  final String subtitle;
+
+  SeminarListTile(this.title, this.subtitle);
+
+  @override
+  Widget buildSubtitle(BuildContext context) => Text(subtitle);
+
+  @override
+  Widget buildTitle(BuildContext context) => Text(title);
 }
